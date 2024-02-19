e815384191817c98fef18bcc8a2c5ae447fe2148
wendellast <wendelalves01a@hotmail.com>
2024-02-19 14:39:13 -0300

    test-banner

--- product/admin.py
+++ product/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from . import models
 
 
+
+
 class Variationline(admin.TabularInline):
     model = models.Variation
     extra = 1
@@ -17,5 +19,15 @@ class ProdutoAdmin(admin.ModelAdmin):
     inlines = [Variationline]
 
 
+class ImagemBannerInline(admin.TabularInline):
+    model = models.ImagemBanner.banners.through
+    extra = 1
+
+class BannerAdmin(admin.ModelAdmin):
+    inlines = [ImagemBannerInline]
+
+
 admin.site.register(models.Product, ProdutoAdmin)
+admin.site.register(models.Banner, BannerAdmin)
+admin.site.register(models.ImagemBanner)
 admin.site.register(models.Variation)
