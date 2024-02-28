d97d5ed46128d5f7237d219644e7dc5ed6334b5b
Andra-sun <camileguimaraes463@gmail.com>
2024-02-28 18:11:27 -0300

    cart atualizado

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "34s66f2twc8c&q*i_@@4i(yp7wp0-d_hi3il7%1iom55xz(87w"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-b8d4.up.railway.app']
+ALLOWED_HOSTS = ['web-production-b8d4.up.railway.app', "127.0.0.1"]
 
 
 # Application definition
