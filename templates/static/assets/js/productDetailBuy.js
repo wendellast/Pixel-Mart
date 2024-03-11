07473af12f77b169edb46a66c72914b1b743b271
Andra-sun <camileguimaraes463@gmail.com>
2024-03-11 12:09:32 -0300

    atualizei

--- templates/static/assets/js/productDetailBuy.js
+++ templates/static/assets/js/productDetailBuy.js
@@ -9,18 +9,42 @@ mais.addEventListener("click", () => {
 
 menos.addEventListener("click", () => {
     let currentValue = parseInt(visor.textContent);
-    if (currentValue > 0) {
+    if (currentValue > 1) {
         visor.textContent = currentValue - 1;
     }
 });
 
-const x = document.getElementById("x");
-const tela = document.getElementById("verMais");
+const xClose = document.getElementById("x");
+const tela = document.getElementsByClassName("verMais")[0];
 const verMais = document.getElementById("abrirVerMais");
 
 verMais.addEventListener("click", function () {
     tela.style.display = "flex";
 });
-x.addEventListener("click", function () {
+xClose.addEventListener("click", function () {
     tela.style.display = "none";
+});
+
+const mainImage = document.getElementsByClassName("mainImg")[0];
+const smallerImages = document.querySelectorAll(".menores .imgMenor img");
+
+smallerImages.forEach((smallerImage) => {
+
+  smallerImage.addEventListener("click", () => {
+
+    mainImage.src = smallerImage.src;
+
+  });
+
+});
+
+const closeVar = document.getElementById("closeVar");
+const selectScreen = document.getElementsByClassName("SelectVariation")[0];
+const openSelect = document.getElementsByClassName("add")[0];
+
+openSelect.addEventListener("click", function () {
+    selectScreen.style.display = "flex";
+});
+closeVar.addEventListener("click", function () {
+    selectScreen.style.display = "none";
 });
