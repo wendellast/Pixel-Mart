6824fc8663f6b7b852dc2ee4330ce7c624d066ff
wendellast <wendelalves01a@hotmail.com>
2024-03-17 22:18:48 -0300

    yesy

--- product/admin.py
+++ product/admin.py
@@ -7,6 +7,28 @@ class Variationline(admin.TabularInline):
     extra = 1
 
 
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'name', 'preco', 'preco_promotional', 'estoque')
+    list_filter = ('product', 'name')
+    search_fields = ('name', 'product__name')
+
+class VariationImageAdmin(admin.ModelAdmin):
+    list_display = ['image_var']
+
+
+
+
+
+
+
+
+class ProductTableLineInline(admin.TabularInline):
+    model = models.ProductTabell
+    extra = 1
+
+
+
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = [
         "name",
@@ -14,8 +36,13 @@ class ProdutoAdmin(admin.ModelAdmin):
         "price_marketing",
         "price_marketing_promotional",
     ]
-    inlines = [Variationline]
+    inlines = [Variationline, ProductTableLineInline]
 
 
+
+
+admin.site.register(models.VariationImage, VariationImageAdmin)
+admin.site.register(models.Variation, VariationAdmin)
+admin.site.register(models.ProductTabell)
 admin.site.register(models.Product, ProdutoAdmin)
-admin.site.register(models.Variation)
+
--- product/models.py
+++ product/models.py
@@ -61,13 +61,24 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class ProductTabell(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    information = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+
+class VariationImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    image_var = models.ImageField(upload_to="product_image_var/%Y/%m/", blank=True, null=True)
 
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, blank=True, null=True)
     preco = models.FloatField()
+    image_var = models.ManyToManyField(VariationImage)
     preco_promotional = models.FloatField(default=0)
     estoque = models.PositiveIntegerField(default=1)
+
 
     def _str__(self):
         return self.name or self.product.name
